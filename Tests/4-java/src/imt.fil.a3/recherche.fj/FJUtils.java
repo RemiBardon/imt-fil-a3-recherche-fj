36fa8add0afac8f9330cd2cf481586fb6d60ae40
Lucas ROURET <lucas.rouretperso@gmail.com>
2022-11-12 11:36:53 +0100

    isSubtype done

--- Tests/4-java/src/imt.fil.a3/recherche.fj/FJUtils.java
+++ Tests/4-java/src/imt.fil.a3/recherche.fj/FJUtils.java
@@ -2,6 +2,8 @@ package imt.fil.a3.recherche.fj;
 
 import imt.fil.a3.recherche.fj.parser.*;
 import imt.fil.a3.recherche.fj.parser.expression.FJExpr;
+import imt.fil.a3.recherche.fj.parser.type.FJClass;
+import imt.fil.a3.recherche.fj.parser.type.FJInterface;
 import imt.fil.a3.recherche.fj.parser.type.FJType;
 
 import java.util.HashMap;
@@ -11,8 +13,33 @@ import java.util.Optional;
 
 public class FJUtils {
 
-    public static Boolean isSubtype(HashMap<String, FJType> classTable, String type1, String type2) {
-        throw new RuntimeException("Not implemented yet.");
+    public static Boolean isSubtype(HashMap<String, FJType> classTable, String classA, String classB) {
+        if(classA.equals(classB)) return true;
+
+        FJType fjType = classTable.get(classA);
+
+        if(fjType instanceof FJClass){
+            FJClass fjClass = (FJClass) fjType;
+            if( fjClass.extendsName.equals(classB) || fjClass.implementsNames.contains(classB)){
+                return true;
+            }else{
+                return isSubtype(classTable,fjClass.extendsName,classB) ||
+                        fjClass.implementsNames.stream()
+                        .filter(implementName -> isSubtype(classTable,implementName,classB))
+                        .findFirst()
+                        .isPresent();
+            }
+        } else if (fjType instanceof FJInterface) {
+            FJInterface fjInterface = (FJInterface) fjType;
+            return fjInterface.extendsNames.contains(classB) ||
+                    fjInterface.extendsNames.stream()
+                            .filter(implementName -> isSubtype(classTable,implementName,classB))
+                            .findFirst()
+                            .isPresent();
+        }
+        else{
+            return false;
+        }
     }
 
     public static Optional<List<FJField>> classFields(
