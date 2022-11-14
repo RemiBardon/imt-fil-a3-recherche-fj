858cec19788926ede54395be14ce3c69eccb94f8
Rémi Bardon <remi.bardon.dev@gmail.com>
2022-11-14 08:32:24 +0100

    🚧 WIP Lint and improve `FJUtils`

--- Tests/4-java/src/imt.fil.a3/recherche.fj/FJUtils.java
+++ Tests/4-java/src/imt.fil.a3/recherche.fj/FJUtils.java
@@ -1,154 +1,134 @@
 package imt.fil.a3.recherche.fj;
 
 import imt.fil.a3.recherche.fj.parser.*;
-import imt.fil.a3.recherche.fj.parser.expression.FJExpr;
-import imt.fil.a3.recherche.fj.parser.type.FJClass;
-import imt.fil.a3.recherche.fj.parser.type.FJInterface;
-import imt.fil.a3.recherche.fj.parser.type.FJType;
+import imt.fil.a3.recherche.fj.parser.expression.*;
+import imt.fil.a3.recherche.fj.parser.type.*;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-
 public class FJUtils {
 
-    public static Boolean isSubtype(HashMap<String, FJType> classTable, String classA, String classB) {
-        if(classA.equals(classB)) return true;
+    public static Boolean isSubtype(
+        final HashMap<String, FJType> classTable,
+        final String classA,
+        final String classB
+    ) {
+        if (classA.equals(classB)) return true;
 
-        FJType fjType = classTable.get(classA);
+        final FJType fjType = classTable.get(classA);
 
-        if(fjType instanceof FJClass){
-            FJClass fjClass = (FJClass) fjType;
-            if( fjClass.extendsName.equals(classB) || fjClass.implementsNames.contains(classB)){
+        if (fjType instanceof final FJClass fjClass) {
+            if (fjClass.extendsName.equals(classB) || fjClass.implementsNames.contains(classB)) {
                 return true;
-            }else{
-                return isSubtype(classTable,fjClass.extendsName,classB) ||
-                        fjClass.implementsNames.stream()
-                        .filter(implementName -> isSubtype(classTable,implementName,classB))
-                        .findFirst()
-                        .isPresent();
+            } else {
+                return isSubtype(classTable, fjClass.extendsName, classB)
+                    || fjClass.implementsNames.stream()
+                        .anyMatch(implementName -> isSubtype(classTable, implementName, classB));
             }
-        } else if (fjType instanceof FJInterface) {
-            FJInterface fjInterface = (FJInterface) fjType;
-            return fjInterface.extendsNames.contains(classB) ||
-                    fjInterface.extendsNames.stream()
-                            .filter(implementName -> isSubtype(classTable,implementName,classB))
-                            .findFirst()
-                            .isPresent();
-        }
-        else{
+        } else if (fjType instanceof final FJInterface fjInterface) {
+            return fjInterface.extendsNames.contains(classB)
+                || fjInterface.extendsNames.stream()
+                    .anyMatch(implementName -> isSubtype(classTable, implementName, classB));
+        } else {
             return false;
         }
     }
 
     public static Optional<List<FJField>> classFields(
-        HashMap<String, FJType> classTable,
-        String className
+        final HashMap<String, FJType> classTable,
+        final String className
     ) {
-        if(className.equals("Object")) return Optional.empty();
+        if (className.equals("Object")) return Optional.empty();
 
         FJType fjType = classTable.get(className);
 
-        if(fjType instanceof FJClass){
-            FJClass fjClass = (FJClass) fjType;
-            return classFields(classTable,fjClass.extendsName)
-                    .map(fields -> {
-                        fields.addAll(fjClass.fields);
-                        return fields;
-                    });
-        }
-        else{
+        if (fjType instanceof final FJClass fjClass) {
+            return classFields(classTable, fjClass.extendsName).map(fields -> {
+                fields.addAll(fjClass.fields);
+                return fields;
+            });
+        } else {
             return Optional.empty();
         }
     }
 
     public static Optional<List<FJSignature>> abstractMethods(
-        HashMap<String, FJType> classTable,
-        String className
+        final HashMap<String, FJType> classTable,
+        final String className
     ) {
-        if(className.equals("Object")) return Optional.empty();
+        if (className.equals("Object")) return Optional.empty();
 
-        FJType fjType = classTable.get(className);
+        final FJType fjType = classTable.get(className);
 
-        if(fjType instanceof FJClass){
-            FJClass fjClass = (FJClass) fjType;
-
+        if (fjType instanceof final FJClass fjClass) {
             return abstractMethods(classTable,fjClass.extendsName)
-                    .map(superAbstractMethods -> {
-                        List<String> implementsAbstractMethods = fjClass.implementsNames.stream()
-                                .map(implementName -> abstractMethods(classTable,implementName))
-                                .filter(Optional::isPresent)
-                                .map(Optional::get)
-                                .flatMap(List::stream)
-                                .map(FJSignature::toString)
-                                .collect(Collectors.toList());
+                .map(superAbstractMethods -> {
+                    final List<String> implementsAbstractMethods = fjClass.implementsNames.stream()
+                        .map(implementName -> abstractMethods(classTable, implementName))
+                        .flatMap(Optional::stream)
+                        .flatMap(List::stream)
+                        .map(FJSignature::toString)
+                        .toList();
 
+                    // union between superAbstractMethods and implementsAbstractMethods when they are in the same class
+                    final List<FJSignature> abstractMethods = superAbstractMethods.stream()
+                        .filter(superAbstractMethod -> implementsAbstractMethods.contains(superAbstractMethod.name))
+                        .toList();
 
-                        //union between superAbstractMethods and implementsAbstractMethods when they are in the same class
-                        List<FJSignature> abstractMethods = superAbstractMethods.stream()
-                                .filter(superAbstractMethod -> implementsAbstractMethods.contains(superAbstractMethod.name))
-                                .collect(Collectors.toList());
+                    // final List<FJSignature> concreteMethods
 
-                        List<FJSignature> concreteMethods
-                    });
-
-
+                    throw new RuntimeException("Not implemented yet.");
+                });
         }
-
-
-
-
+        throw new RuntimeException("Not implemented yet.");
     }
 
     public static Optional<List<FJMethod>> methods(
-        HashMap<String, FJType> classTable,
-        String className
+        final HashMap<String, FJType> classTable,
+        final String className
     ) {
-        if(className.equals("Object")) return Optional.empty();
+        if (className.equals("Object")) return Optional.empty();
 
-        FJType fjType = classTable.get(className);
+        final FJType fjType = classTable.get(className);
 
-        if(fjType instanceof FJClass){
-            FJClass fjClass = (FJClass) fjType;
-            return methods(classTable,fjClass.extendsName)
-                    .map(methods -> {
-                        methods.addAll(fjClass.methods);
-                        return methods;
-                    });
-        }
-        else{
+        if (fjType instanceof final FJClass fjClass) {
+            return methods(classTable,fjClass.extendsName).map(methods -> {
+                methods.addAll(fjClass.methods);
+                return methods;
+            });
+        } else {
             return Optional.empty();
         }
-
     }
 
     public static Optional<FJMethodTypeSignature> methodType(
-        HashMap<String, FJType> classTable,
-        String methodName,
-        String className
+        final HashMap<String, FJType> classTable,
+        final String methodName,
+        final String className
     ) {
         throw new RuntimeException("Not implemented yet.");
     }
 
     public static Optional<FJMethodBodySignature> methodBody(
-        HashMap<String, FJType> classTable,
-        String methodName,
-        String className
+        final HashMap<String, FJType> classTable,
+        final String methodName,
+        final String className
     ) {
         throw new RuntimeException("Not implemented yet.");
     }
 
-    public static Boolean isValue(HashMap<String, FJType> classTable, FJExpr expr) {
+    public static Boolean isValue(final HashMap<String, FJType> classTable, final FJExpr expr) {
         throw new RuntimeException("Not implemented yet.");
     }
 
-    public static FJExpr lambdaMark(FJExpr expr, String type) {
+    public static FJExpr lambdaMark(final FJExpr expr, final String type) {
         throw new RuntimeException("Not implemented yet.");
     }
 
-    public static FJExpr removeRuntimeAnnotation(FJExpr expr) {
+    public static FJExpr removeRuntimeAnnotation(final FJExpr expr) {
         throw new RuntimeException("Not implemented yet.");
     }
 }
