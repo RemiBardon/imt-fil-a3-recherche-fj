984a76fc796695f7f6414cef3c8ce5841dc636b6
Lucas ROURET <lucas.rouretperso@gmail.com>
2022-11-14 19:47:55 +0100

    WIP FJUtils

--- Explorations/4-java/src/imt.fil.a3/recherche.fj/FJUtils.java
+++ Explorations/4-java/src/imt.fil.a3/recherche.fj/FJUtils.java
@@ -54,6 +54,7 @@ public class FJUtils {
         }
     }
 
+
     public static Optional<List<FJSignature>> abstractMethods(
         final HashMap<String, FJType> classTable,
         final String className
@@ -63,26 +64,62 @@ public class FJUtils {
         final FJType fjType = classTable.get(className);
 
         if (fjType instanceof final FJClass fjClass) {
-            return abstractMethods(classTable,fjClass.extendsName)
-                .map(superAbstractMethods -> {
-                    final List<String> implementsAbstractMethods = fjClass.implementsNames.stream()
-                        .map(implementName -> abstractMethods(classTable, implementName))
-                        .flatMap(Optional::stream)
-                        .flatMap(List::stream)
-                        .map(FJSignature::toString)
-                        .toList();
+            //get methods from implemented interfaces
+            final Optional<List<FJMethod>> methodsFromImplemnts = fjClass.implementsNames.stream()
+                    .map(implementName -> methods(classTable, implementName))
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
+                    .reduce((methods1, methods2) -> {
+                        methods1.addAll(methods2);
+                        return methods1;
+                    });
 
-                    // union between superAbstractMethods and implementsAbstractMethods when they are in the same class
-                    final List<FJSignature> abstractMethods = superAbstractMethods.stream()
-                        .filter(superAbstractMethod -> implementsAbstractMethods.contains(superAbstractMethod.name))
-                        .toList();
+            //get methods from abstract methods
+            final Optional<List<FJMethod>> methodsFromAbstract = abstractMethods(classTable, className)
+                    .map(abstractMethods -> abstractMethods.stream()
+                            .map(abstractMethod -> new FJMethod(
+                                    abstractMethod.returnType,
+                                    abstractMethod.name,
+                                    abstractMethod.arguments,
+                                    new FJBlock(List.of(), List.of())
+                            ))
+                            .toList()
+                    );
 
-                    // final List<FJSignature> concreteMethods
+            //get methods from class
+            final Optional<List<FJMethod>> methodsFromClass = Optional.of(fjClass.methods);
 
-                    throw new RuntimeException("Not implemented yet.");
-                });
+            //merge all methods
+            return methodsFromImplemnts.map(methods -> {
+                methodsFromAbstract.ifPresent(methods::addAll);
+                methodsFromClass.ifPresent(methods::addAll);
+                return methods;
+            });
+        } else if( fjType instanceof final FJInterface fjInterface) {
+            //get super methods abstract
+            final Optional<List<FJMethod>> methodsFromAbstract = abstractMethods(classTable, className)
+                    .map(abstractMethods -> abstractMethods.stream()
+                            .map(abstractMethod -> new FJMethod(
+                                    abstractMethod.returnType,
+                                    abstractMethod.name,
+                                    abstractMethod.arguments,
+                                    new FJBlock(List.of(), List.of())
+                            ))
+                            .toList()
+                    );
+
+            //get abstract methods from interface
+            final Optional<List<FJMethod>> methodsFromInterface = Optional.of(fjInterface.methods);
+
+            //merge all methods
+            return methodsFromAbstract.map(methods -> {
+                methodsFromInterface.ifPresent(methods::addAll);
+                return methods;
+            });
+        } else {
+            return Optional.empty();
         }
-        throw new RuntimeException("Not implemented yet.");
+
     }
 
     public static Optional<List<FJMethod>> methods(
@@ -94,8 +131,34 @@ public class FJUtils {
         final FJType fjType = classTable.get(className);
 
         if (fjType instanceof final FJClass fjClass) {
-            return methods(classTable,fjClass.extendsName).map(methods -> {
-                methods.addAll(fjClass.methods);
+           //get methods from implemented interfaces
+            final Optional<List<FJMethod>> methodsFromImplemnts = fjClass.implementsNames.stream()
+                    .map(implementName -> methods(classTable, implementName))
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
+                    .reduce((methods1, methods2) -> {
+                        methods1.addAll(methods2);
+                        return methods1;
+                    });
+
+            //get methods from class
+            final Optional<List<FJMethod>> methodsFromClass = Optional.of(fjClass.methods);
+
+            //merge all methods
+            return methodsFromImplemnts.map(methods -> {
+                methodsFromClass.ifPresent(methods::addAll);
+                return methods;
+            });
+        } else if( fjType instanceof final FJInterface fjInterface) {
+            //get super methods
+            final Optional<List<FJMethod>> methodsFromSuper = methods(classTable, className);
+
+            //get methods from interface
+            final Optional<List<FJMethod>> methodsFromInterface = Optional.of(fjInterface.methods);
+
+            //merge all methods
+            return methodsFromSuper.map(methods -> {
+                methodsFromInterface.ifPresent(methods::addAll);
                 return methods;
             });
         } else {
@@ -108,7 +171,26 @@ public class FJUtils {
         final String methodName,
         final String className
     ) {
-        throw new RuntimeException("Not implemented yet.");
+        if (className.equals("Object")) return Optional.empty();
+
+        //get abstract methods
+        final Optional<List<FJSignature>> abstractMethods = abstractMethods(classTable, className);
+
+//get methods
+        final Optional<List<FJMethod>> methods = methods(classTable, className);
+
+        //get method
+        final Optional<FJMethod> method = methods.flatMap(methods1 -> methods1.stream()
+                .filter(method1 -> method1.name.equals(methodName))
+                .findFirst());
+
+        //get method type
+        return method.map(method1 -> new FJMethodTypeSignature(
+                method1.returnType,
+                method1.arguments.stream()
+                        .map(argument -> argument.type)
+                        .toList()
+        ));
     }
 
     public static Optional<FJMethodBodySignature> methodBody(
@@ -116,6 +198,21 @@ public class FJUtils {
         final String methodName,
         final String className
     ) {
-        throw new RuntimeException("Not implemented yet.");
+        if (className.equals("Object")) return Optional.empty();
+
+        //get methods
+        final Optional<List<FJMethod>> methods = methods(classTable, className);
+
+        //get method
+        final Optional<FJMethod> method = methods.flatMap(methods1 -> methods1.stream()
+                .filter(method1 -> method1.name.equals(methodName))
+                .findFirst());
+
+        //get method body
+        return method.map(method1 -> new FJMethodBodySignature(
+                method1.returnType,
+                method1.arguments,
+                method1.block
+        ));
     }
 }
