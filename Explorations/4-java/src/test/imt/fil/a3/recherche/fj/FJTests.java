6e92a279a293b5fa272a95d783ef6b0df4c7ecca
Rémi Bardon <remi.bardon.dev@gmail.com>
2022-11-18 11:17:16 +0100

    ✅ Introduce JUnit

--- Explorations/4-java/src/test/imt/fil/a3/recherche/fj/FJTests.java
+++ Explorations/4-java/src/test/imt/fil/a3/recherche/fj/FJTests.java
@@ -4,75 +4,77 @@ import imt.fil.a3.recherche.fj.model.TypeCheckingContext;
 import imt.fil.a3.recherche.fj.model.java.misc.FJProgram;
 import imt.fil.a3.recherche.fj.util.builder.FJProgramBuilder;
 import imt.fil.a3.recherche.fj.util.builder.error.FJBuilderException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 
 @SuppressWarnings("SpellCheckingInspection")
-public class FJTests {
+final class FJTests {
 
-    public static void main(String[] args) {
-        try {
-            FJProgram program = new FJProgramBuilder()
-                .clazz(c -> c
+    @Test
+    void testPaperExampleTypeChecks() {
+        final FJProgramBuilder programBuilder = new FJProgramBuilder()
+            .clazz(c -> c
+                .name("A")
+                .constructor(cb -> cb
                     .name("A")
-                    .constructor(cb -> cb
-                        .name("A")
-                    )
                 )
-                .clazz(c -> c
+            )
+            .clazz(c -> c
+                .name("B")
+                .constructor(cb -> cb
                     .name("B")
-                    .constructor(cb -> cb
-                        .name("B")
+                )
+            )
+            .clazz(c -> c
+                .name("Pair")
+                .constructor(cb -> cb
+                    .name("Pair")
+                    .arg(fb -> fb.type("A").name("fst"))
+                    .arg(fb -> fb.type("B").name("snd"))
+                    .fieldInit(fib -> fib
+                        .fieldName("fst")
+                        .argumentName("fst")
+                    )
+                    .fieldInit(fib -> fib
+                        .fieldName("snd")
+                        .argumentName("snd")
                     )
                 )
-                .clazz(c -> c
-                    .name("Pair")
-                    .constructor(cb -> cb
-                        .name("Pair")
-                        .arg(fb -> fb.type("A").name("fst"))
-                        .arg(fb -> fb.type("B").name("snd"))
-                        .fieldInit(fib -> fib
-                            .fieldName("fst")
-                            .argumentName("fst")
-                        )
-                        .fieldInit(fib -> fib
-                            .fieldName("snd")
-                            .argumentName("snd")
-                        )
+                .field(fb -> fb.type("A").name("fst"))
+                .field(f -> f.type("B").name("snd"))
+                .method(mb -> mb
+                    .signature(sb -> sb
+                        .returnTypeName("Pair")
+                        .name("setfst")
+                        .arg(fb -> fb.type("A").name("newfst"))
                     )
-                    .field(fb -> fb.type("A").name("fst"))
-                    .field(f -> f.type("B").name("snd"))
-                    .method(mb -> mb
-                        .signature(sb -> sb
-                            .returnTypeName("Pair")
-                            .name("setfst")
-                            .arg(fb -> fb.type("A").name("newfst"))
-                        )
-                        // return new Pair ( newfst , this . snd )
-                        .body(eb -> eb
-                            .createObject(cob -> cob
-                                .className("Pair")
-                                .arg(ebArg -> ebArg
-                                    .variable(vb -> vb.name("newfst"))
-                                )
-                                .arg(ebArg -> ebArg
-                                    .fieldAccess(fab -> fab
-                                        .fieldName("snd")
-                                        .object(ebObject -> ebObject
-                                            .variable(vb -> vb.name("this"))
-                                        )
+                    // return new Pair ( newfst , this . snd )
+                    .body(eb -> eb
+                        .createObject(cob -> cob
+                            .className("Pair")
+                            .arg(ebArg -> ebArg
+                                .variable(vb -> vb.name("newfst"))
+                            )
+                            .arg(ebArg -> ebArg
+                                .fieldAccess(fab -> fab
+                                    .fieldName("snd")
+                                    .object(ebObject -> ebObject
+                                        .variable(vb -> vb.name("this"))
                                     )
                                 )
                             )
                         )
                     )
                 )
-                .build();
+            );
+        try {
+            FJProgram program = programBuilder.build();
             final var context = new TypeCheckingContext(program.getTypeTable(), new HashMap<>());
-            System.out.println(program.typeCheck(context));
+            Assertions.assertTrue(program.typeCheck(context));
         } catch (FJBuilderException e) {
-            e.printStackTrace();
-            assert false;
+            Assertions.fail(e);
         }
     }
 
